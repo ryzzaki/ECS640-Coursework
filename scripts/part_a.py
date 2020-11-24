1fc2078f7259b759d324738cbc8cd1f2222c744a
ryzzaki <cuong.nugi@gmail.com>
2020-11-24 14:12:56 +0100

    adding a try block

--- scripts/part_a.py
+++ scripts/part_a.py
@@ -4,13 +4,16 @@ from datetime import datetime
 
 class PartA(MRJob):
     def mapper(self, _, transaction):
-        # split the block of transaction and select timestamp
-        block_timestamp = transaction.split(',')[-1]
-        # convert the timestamp into a unified format of %month-%year (e.g. 10-2020)
-        year_month_key = datetime.utcfromtimestamp(
-            int(block_timestamp)).strftime('%m-%Y')
-        # yield the result with the count of 1
-        yield(year_month_key, 1)
+        try:
+            # split the block of transaction and select timestamp
+            block_timestamp = transaction.split(',')[-1]
+            # convert the timestamp into a unified format of %month-%year (e.g. 10-2020)
+            year_month_key = datetime.utcfromtimestamp(
+                int(block_timestamp)).strftime('%m-%Y')
+            # yield the result with the count of 1
+            yield(year_month_key, 1)
+        except:
+            pass
 
     def reducer(self, year_month_key, counts):
         yield(year_month_key, sum(counts))
