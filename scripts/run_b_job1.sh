7cbd187f2efefe50ca7e60419dc2dc52a9e46a59
ryzzaki <cuong.nugi@gmail.com>
2020-12-04 16:48:00 +0100

    add contracts dataset

--- scripts/run_b_job1.sh
+++ scripts/run_b_job1.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python ./scripts/part_b_job1.py -r hadoop hdfs://andromeda.eecs.qmul.ac.uk/data/ethereum/transactions > ./output/part_b_out.txt
+python ./scripts/part_b_job1.py -r hadoop hdfs://andromeda.eecs.qmul.ac.uk/data/ethereum/transactions hdfs://andromeda.eecs.qmul.ac.uk/data/ethereum/contracts > ./output/part_b_out.txt
