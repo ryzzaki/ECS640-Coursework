68d91d57571ba601551a3ec5d7ab9df29c2ee3ad
ryzzaki <cuong.nugi@gmail.com>
2020-12-04 17:18:52 +0100

    fix script

--- scripts/run_b.sh
+++ scripts/run_b.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python ./scripts/part_b_job1.py -r hadoop --output-dir output/partb --no-cat-output hdfs://andromeda.eecs.qmul.ac.uk/data/ethereum/transactions hdfs://andromeda.eecs.qmul.ac.uk/data/ethereum/contracts
+python ./scripts/part_b.py -r hadoop --output-dir output/partb --no-cat-output hdfs://andromeda.eecs.qmul.ac.uk/data/ethereum/transactions hdfs://andromeda.eecs.qmul.ac.uk/data/ethereum/contracts
