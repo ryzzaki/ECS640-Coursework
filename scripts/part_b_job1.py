33ba624ecccd8c7111ca7c5f3802f0cb394ee70d
ryzzaki <cuong.nugi@gmail.com>
2020-12-04 19:40:26 +0100

    splitting part b into multiple jobs

---
+++ scripts/part_b_job1.py
@@ -0,0 +1,25 @@
+from mrjob.job import MRJob
+
+
+class PartB_Job1(MRJob):
+    def mapper(self, _, row):
+        try:
+            splits = row.split(',')
+            if len(splits) == 7:
+                to_address = splits[2]
+                value = int(splits[3])
+                if value != 0:
+                    yield(to_address, value)
+        except:
+            pass
+
+    def combiner(self, address, values):
+        yield(address, sum(values))
+
+    def reducer(self, address, values):
+        yield(address, sum(values))
+
+
+if __name__ == "__main__":
+    PartB_Job1.JOBCONF = {'mapreduce.job.reduces': '4'}
+    PartB_Job1.run()
---
+++ scripts/run_b_job1.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+python ./scripts/part_b_job1.py -r hadoop --output-dir output/partbj1 --no-cat-output hdfs://andromeda.eecs.qmul.ac.uk/data/ethereum/transactions 
