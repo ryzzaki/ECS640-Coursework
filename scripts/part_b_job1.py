19026794cfc197255fa5003b2e0c37a7ef7fa96a
ryzzaki <cuong.nugi@gmail.com>
2020-12-04 16:44:12 +0100

    added steps

--- scripts/part_b_job1.py
+++ scripts/part_b_job1.py
@@ -1,8 +1,9 @@
 from mrjob.job import MRJob
+from mrjob.step import MRStep
 
 
 class PartB_Job1(MRJob):
-    def mapper(self, _, row):
+    def mapper_repartition_init(self, _, row):
         try:
             splits = row.split(',')
             if len(splits) == 7:
@@ -14,31 +15,67 @@ class PartB_Job1(MRJob):
             elif len(splits) == 5:
                 # this is a contract
                 sc_address = splits[0]
-                is_erc20 = splits[1]
-                is_erc721 = splits[2]
-                yield(sc_address, ["sc", is_erc20, is_erc721])
+                yield(sc_address, "sc")
             else:
                 pass
         except:
             pass
 
-    def combiner(self, address, values):
-        values = [x for x in values]
-        if len(values):
-            value_type = values[0]
-            for value in values[1:]:
-                yield(address, [value_type, value])
-        else:
+    def combiner_repartition_init(self, address, values):
+        try:
+            has_sc = False
+            transacted_amount = 0
+            # loop through the values and count the transacted amounts in smart contracts
+            for value in values:
+                if value[0] == "tsc":
+                    transacted_amount += value[1]
+                elif value[0] == "sc":
+                    has_sc = True
+            # only yield if this is a smart contract
+            if has_sc is True:
+                yield(address, transacted_amount)
+        except:
             pass
 
-    def reducer(self, address, values):
-        values = [x for x in values]
-        if len(values):
-            value_type = values[0]
-            for value in values[1:]:
-                yield(address, [value_type, value])
-        else:
+    def reducer_repartition_init(self, address, values):
+        try:
+            has_sc = False
+            transacted_amount = 0
+            # loop through the values and count the transacted amounts in smart contracts
+            for value in values:
+                if value[0] == "tsc":
+                    transacted_amount += value[1]
+                elif value[0] == "sc":
+                    has_sc = True
+            # only yield if this is a smart contract
+            if has_sc is True:
+                yield(address, transacted_amount)
+        except:
             pass
+
+    def mapper_aggregate(self, address, ts_amount):
+        yield(None, (address, ts_amount))
+
+    def combiner_aggregate(self, _, values):
+        i = 0
+        sorted_values = sorted(values, reverse=True, key=lambda tup: tup[1])
+        for value in sorted_values:
+            yield(None, value)
+            i += 1
+            if i >= 10:
+                break
+
+    def reducer_aggregate(self, _, values):
+        i = 0
+        sorted_values = sorted(values, reverse=True, key=lambda tup: tup[1])
+        for value in sorted_values:
+            yield("{} - {}".format(value[0], value[1]), None)
+            i += 1
+            if i >= 10:
+                break
+
+    def steps(self):
+        return [MRStep(mapper=self.mapper_repartition_init, combiner=self.combiner_repartition_init, reducer=self.reducer_repartition_init), MRStep(mapper=self.mapper_aggregate, combiner=self.combiner_aggregate, reducer=self.reducer_aggregate)]
 
 
 if __name__ == "__main__":
