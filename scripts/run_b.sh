b98acbbf53deb1a0ca0af2c427cd4369f1e41a7d
ryzzaki <cuong.nugi@gmail.com>
2020-12-06 17:19:38 +0000

    part b results & adjusted script

--- scripts/run_b.sh
+++ scripts/run_b.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-hadoop fs -rm -r -f output/partb && python ./scripts/part_b.py -r hadoop --output-dir output/partb --no-cat-output hdfs://andromeda.eecs.qmul.ac.uk/data/ethereum/transactions hdfs://andromeda.eecs.qmul.ac.uk/data/ethereum/contracts
+python ./scripts/part_b.py -r hadoop hdfs://andromeda.eecs.qmul.ac.uk/data/ethereum/transactions hdfs://andromeda.eecs.qmul.ac.uk/data/ethereum/contracts > ./output/part_b_out.txt
