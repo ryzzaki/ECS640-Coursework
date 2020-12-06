5ded478d8753cd7110d442a9b1a6fe25d8e4fe3b
ryzzaki <cuong.nugi@gmail.com>
2020-12-06 18:22:50 +0100

    add snapshot dataset test files

--- scripts/run_b_job1.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-hadoop fs -rm -r -f output/partbj1 && python ./scripts/part_b_job1.py -r hadoop --output-dir output/partbj1 --no-cat-output hdfs://andromeda.eecs.qmul.ac.uk/data/ethereum/transactions
--- scripts/setup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-module load python/3.7.7 && source env/bin/activate
