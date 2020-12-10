eac3030f123bf7b92360ec4184247f618d52e5b0
ryzzaki <cuong.nugi@gmail.com>
2020-12-10 11:17:44 +0100

    add another step for scam aggregation

--- scripts/part_d_scams.py
+++ scripts/part_d_scams.py
@@ -13,14 +13,12 @@ class PartD(MRJob):
             scams_json = parsed_json["result"].items()
             for _, v in scams_json:
                 category = v["category"]
-                coin = v["coin"]
                 addresses = v["addresses"]
-                if coin == "ETH":
-                    if category in self.eth_scams:
-                        self.eth_scams[category] = list(
-                            set(self.eth_scams[category] + addresses))
-                    else:
-                        self.eth_scams[category] = addresses
+                if category in self.eth_scams:
+                    self.eth_scams[category] = list(
+                        set(self.eth_scams[category] + addresses))
+                else:
+                    self.eth_scams[category] = addresses
             f.close()
 
     def mapper_category_aggregate(self, _, transaction):
@@ -36,11 +34,42 @@ class PartD(MRJob):
             for k, v in self.eth_scams.items():
                 if to_address in set(v):
                     # yields (year_month_key, (category, value))
-                    yield(year_month_key, (k, value))
+                    yield(k, (year_month_key, value))
         except:
             pass
 
-    def combiner_category_aggregate(self, year_month_key, values):
+    def combiner_category_aggregate(self, category, values):
+        values = [x for x in values]
+        data_types = {}
+        for arr in values:
+            key = arr[0]
+            vals = [arr[1]]
+            if key in data_types:
+                data_types[key] = [sum(v)
+                                   for v in zip(data_types[key], vals)]
+            else:
+                data_types[key] = vals
+        for data_type in data_types.items():
+            yield(category, (data_type[0], data_type[1].pop()))
+
+    def reducer_category_aggregate(self, category, values):
+        values = [x for x in values]
+        data_types = {}
+        for arr in values:
+            key = arr[0]
+            vals = [arr[1]]
+            if key in data_types:
+                data_types[key] = [sum(v)
+                                   for v in zip(data_types[key], vals)]
+            else:
+                data_types[key] = vals
+        for data_type in data_types.items():
+            yield(category, (data_type[0], data_type[1].pop()))
+
+    def mapper_month_aggregate(self, category, values):
+        yield(values[0], (category, values[1]))
+
+    def combiner_month_aggregate(self, year_month_key, values):
         values = [x for x in values]
         data_types = {}
         for arr in values:
@@ -54,7 +83,7 @@ class PartD(MRJob):
         for data_type in data_types.items():
             yield(year_month_key, (data_type[0], data_type[1].pop()))
 
-    def reducer_category_aggregate(self, year_month_key, values):
+    def reducer_month_aggregate(self, year_month_key, values):
         values = [x for x in values]
         data_types = {}
         for arr in values:
@@ -65,11 +94,14 @@ class PartD(MRJob):
                                    for v in zip(data_types[key], vals)]
             else:
                 data_types[key] = vals
+        scams = []
         for data_type in data_types.items():
-            yield(year_month_key, (data_type[0], data_type[1].pop()))
+            scams.append((data_type[0], data_type[1].pop()))
+        yield(year_month_key, scams)
 
     def steps(self):
-        return [MRStep(mapper_init=self.mapper_join_init, mapper=self.mapper_category_aggregate, combiner=self.combiner_category_aggregate, reducer=self.reducer_category_aggregate)]
+        return [MRStep(mapper_init=self.mapper_join_init, mapper=self.mapper_category_aggregate, combiner=self.combiner_category_aggregate, reducer=self.reducer_category_aggregate),
+                MRStep(mapper=self.mapper_month_aggregate, combiner=self.combiner_month_aggregate, reducer=self.reducer_month_aggregate)]
 
 
 if __name__ == "__main__":
