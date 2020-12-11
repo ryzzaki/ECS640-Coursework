e4789bf4bcb275deb5f452ef4f3277caf19fd9ba
ryzzaki <cuong.nugi@gmail.com>
2020-12-11 22:16:52 +0100

    fix undefined values

--- scripts/part_d_ml.py
+++ scripts/part_d_ml.py
@@ -7,13 +7,30 @@ from datetime import datetime
 sc = SparkContext()
 sparkSession = SparkSession.builder.getOrCreate()
 
+
+def is_good_line(line):
+    try:
+        splits = line.split(',')
+        if len(splits) != 5:
+            return False
+        float(splits[1])
+        float(splits[2])
+        float(splits[3])
+        float(splits[4])
+        return True
+    except:
+        return False
+
+
 data = sparkSession.read.csv(
     "hdfs://andromeda.eecs.qmul.ac.uk/user/vcn01/input/eth_price_inception_to_11_dec.csv", header=True, inferSchema=True)
 
+clean_data = data.filter(is_good_line)
+
 # create features vector
-feature_columns = data.columns[:-1]  # here we omit the final column
+feature_columns = clean_data.columns[:-1]  # here we omit the final column
 assembler = VectorAssembler(inputCols=feature_columns, outputCol="features")
-data_2 = assembler.transform(data)
+data_2 = assembler.transform(clean_data)
 # train/test split
 train, test = data_2.randomSplit([0.7, 0.3])
 # define the model
