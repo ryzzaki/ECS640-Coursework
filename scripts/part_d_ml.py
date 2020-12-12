dfa2e857f228a051b59d12f14957be1590b3d0cb
ryzzaki <cuong.nugi@gmail.com>
2020-12-12 14:03:33 +0100

    fix array vals from tutorial

--- scripts/part_d_ml.py
+++ scripts/part_d_ml.py
@@ -7,27 +7,32 @@ from datetime import datetime
 sc = SparkContext()
 sparkSession = SparkSession.builder.getOrCreate()
 
-
+# read the custom csv file from hdfs into the data frame
 df = sparkSession.read.csv(
     "hdfs://andromeda.eecs.qmul.ac.uk/user/vcn01/input/eth_price_inception_to_11_dec.csv", header=True, inferSchema=False)
 
+# get rid of all undefined values
 clean_data = df.filter((df.open != "undefined") & (df.high != "undefined") & (
     df.low != "undefined") & (df.close != "undefined"))
 
+# recast the columns as floats
+clean_data = clean_data.withColumn("timestamp", clean_data.open.cast('float'))
 clean_data = clean_data.withColumn("open", clean_data.open.cast('float'))
 clean_data = clean_data.withColumn("high", clean_data.open.cast('float'))
 clean_data = clean_data.withColumn("low", clean_data.open.cast('float'))
 clean_data = clean_data.withColumn("close", clean_data.open.cast('float'))
+
+# visual check that data are filtered
 clean_data.show()
 
 # create features vector
-feature_columns = clean_data.columns[:-1]  # here we omit the final column
+feature_columns = clean_data.columns[:-1]  # here we omit the close column
 assembler = VectorAssembler(inputCols=feature_columns, outputCol="features")
 data_2 = assembler.transform(clean_data)
 # train/test split
 train, test = data_2.randomSplit([0.7, 0.3])
 # define the model
-algo = LinearRegression(featuresCol="features", labelCol="open_prices")
+algo = LinearRegression(featuresCol="features", labelCol="close")
 # train the model
 model = algo.fit(train)
 # evaluation
@@ -38,4 +43,4 @@ evaluation_summary.r2
 # predicting values
 predictions = model.transform(test)
 # here I am filtering out some columns just for the figure to fit
-predictions.select(predictions.columns[13:]).show()
+predictions.select(predictions.columns[4:]).show()
